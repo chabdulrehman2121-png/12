84267a22de12fc08b2bed7ed4cb5e3d580395f72
chabdulrehman2121-png <ch.abdul.rehman2121@gmail.com>
2025-11-29 03:09:25 +0500

    Update trains.h

--- PF Project Skeleton/core/trains.h
+++ PF Project Skeleton/core/trains.h
@@ -18,13 +18,13 @@ void spawnTrainsForTick();
 void determineAllRoutes();
 
 // Compute next position/direction for a train.
-bool determineNextPosition();
+bool determineNextPosition(int trainIndex);
 
 // Get next direction on entering a tile.
-int getNextDirection();
+int getNextDirection(int x, int y, int currentDir, int trainIndex);
 
 // Choose best direction at a crossing.
-int getSmartDirectionAtCrossing();
+int getSmartDirectionAtCrossing(int x, int y, int currentDir, int trainIndex);
 
 // ----------------------------------------------------------------------------
 // TRAIN MOVEMENT
